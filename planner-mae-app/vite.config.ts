f9ba1176a4bb434313b3214b5b348eb81a18cf85
Marcelo <app.mascpro@gmail.com>
2026-05-07 21:37:23 -0300

    biblia 2

--- planner-mae-app/vite.config.ts
+++ planner-mae-app/vite.config.ts
@@ -36,17 +36,17 @@ export default defineConfig(({ mode }) => ({
         scope: '/planner-mae/',
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/planner-mae/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icon-512.png',
+            src: '/planner-mae/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/icon-512.png',
+            src: '/planner-mae/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
